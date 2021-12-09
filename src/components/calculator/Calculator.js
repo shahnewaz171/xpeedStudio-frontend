088371da03b2f90272db217d32b2202044ae7f0c
shahnewaz601 <shahnewaz601@gmail.com>
2021-12-10 01:43:31 +0600

    setup react router

---
+++ src/components/calculator/Calculator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Calculator = () => {
+
+    return (
+        <div>
+           <h1>sddsfssdasdsadasd</h1>
+        </div>
+    );
+};
+
+export default Calculator;
