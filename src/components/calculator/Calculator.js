57ae7e5ce39026ebbf5d20b0dcf4008dc50c3716
shahnewaz601 <shahnewaz601@gmail.com>
2021-12-12 11:36:21 +0600

    read file data and added functionality for calculating data

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import fs from 'fs';
 import './Calculator.css';
 
 const Calculator = () => {
+    const [textValue, setTextValue] = useState('');
+    const [fileValue, setFileValue] = useState('');
+    const [result, setResult] = useState('');
+
+    const writtenText = (e) => {
+        const newTextValue = { ...textValue };
+        newTextValue[e.target.name] = e.target.value;
+        setTextValue(newTextValue.text);
+    }
+
+    const handleFile = (e) => {
+        console.log(e.target.files[0]);
+        const reader = new FileReader();
+          reader.onload = function() {
+            const text = reader.result;
+            console.log(text);
+            console.log(__dirname);
+            // fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+            //     if (err) throw err;
+            //     console.log('Saved!');
+            // });
+            // console.log(eval(text));
+        };
+        reader.readAsText(e.target.files[0]);
+    }
+
+    const handleSubmit = (e) => {
+        const re = /^[-+]?\d*\.?\d+(?:[-+*/]?\d+)+?$/;
+        const newTextValue = textValue.replace(/\s/g, '');
+
+        if (re.test(newTextValue)) {
+            const result = eval(newTextValue);
+            console.log(newTextValue);
+            console.log(result);
+        }
+        else {
+            console.log("Something went wrong!");
+        }
+
+        e.preventDefault();
+    }
 
     return (
         <div className="my-5 container custom-container">
@@ -21,10 +63,10 @@ const Calculator = () => {
                         </div>
                         <div className="mt-2 upload">
                             <h3 className="px-3 py-2">Input</h3>
-                            <form className="px-3">
+                            <form className="px-3" onSubmit={handleSubmit}>
                                 <div className="row mb-3">
                                     <div className="col-9">
-                                        <input type="text" name="title" className="form-control" placeholder="Calculation Title" />
+                                        <input type="text" onBlur={writtenText} name="text" className="form-control" placeholder="Calculation Title" required />
                                     </div>
                                     <div className="col-3 d-flex align-items-center">
                                         <p className="text-dark mb-0 text-center">Required</p>
@@ -32,13 +74,13 @@ const Calculator = () => {
                                 </div>
                                 <div className="row pb-4">
                                     <div className="col-9">
-                                        <input type="text" name="title" className="form-control" placeholder="Calculation Title" />
+                                        <input type="file" onChange={handleFile} name="file" className="form-control" accept=".txt" />
                                     </div>
                                     <div className="col-3 d-flex align-items-center">
                                         <p className="text-dark mb-0 text-center">Optional</p>
                                     </div>
                                 </div>
-                                <button className="btn rounded-pill px-4 calculator-btn">Calculate</button>
+                                <button type="submit" className="btn rounded-pill px-4 calculator-btn">Calculate</button>
                             </form>
                         </div>
                     </div>
