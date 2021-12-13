c47191de4d2778f5e1179f713ef9683cdf94c835
shahnewaz601 <shahnewaz601@gmail.com>
2021-12-13 17:12:24 +0600

    fetch calculated data and loaded on the ui

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { FiUploadCloud } from "react-icons/fi";
 import './Calculator.css';
@@ -7,6 +7,15 @@ const Calculator = () => {
     const [writtenValue, setWrittenValue] = useState('');
     const [fileValue, setFileValue] = useState('');
     const [file, setFile] = useState('');
+    const [resultsCard, setResultsCard] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/results')
+        .then(res => {
+            console.log(res.data);
+            setResultsCard(res.data);
+        })
+    }, [])
 
     const writtenText = (e) => {
         const newWrittenValue = { ...writtenValue };
@@ -65,13 +74,25 @@ const Calculator = () => {
                     <div className="mt-3 pb-2 calculator">
                         <div className="px-3 pb-4">
                             <h3 className="pt-3 pb-2">Total results: 03</h3>
-                            <div className="mt-1 py-3 rounded-3 result-card">
-                                <p className="mb-0">
-                                    <span>=24</span>
-                                    <span className="fw-bolder">calculation title</span>
-                                    <span className="preview-btn">See Input</span>
-                                </p>
-                            </div>
+                            {resultsCard?.map(item => {
+                                return (
+                                    <div key={item._id} className="mt-1 py-3 mb-4 rounded-3 result-card">
+                                        <div className="mb-0 d-flex align-items-center">
+                                            <div className="col-md-3">
+                                                <p className="ps-3 mb-0">
+                                                    = <span >{item.output}</span>
+                                                </p>
+                                            </div>
+                                            <div className="col-md-6">
+                                                <p className="fw-bolder mb-0">{item.writtenText}</p>
+                                            </div>
+                                            <div className="col-md-3">
+                                                <p className="preview-btn mb-0">See Input</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })}
                         </div>
                         <div className="mt-2 upload">
                             <h3 className="px-3 py-2">Input</h3>
